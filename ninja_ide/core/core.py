4e2640c5d4f98007f58abb21762d19047aade4e6
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2011-10-27 17:43:58 -0300

    Fixing logging calls.

--- ninja_ide/core/core.py
+++ ninja_ide/core/core.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+from ninja_ide import resources
 from ninja_ide.core import cliparser
-from ninja_ide.gui import ide
 
 
 def run_ninja():
+    """First obtain the execution args and create the resources folder."""
     filenames, projects_path, extra_plugins = cliparser.parse()
+    # Create NINJA-IDE user folder structure for plugins, themes, etc
+    resources.create_home_dir_structure()
+
+    # Start the UI
+    from ninja_ide.gui import ide
     ide.start(filenames, projects_path, extra_plugins)
