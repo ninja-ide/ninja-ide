1e10dad355801b79518fdeb46f51b5a83456c36d
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2012-02-02 01:20:30 -0300

    Fixed Issue #227

--- ninja_ide/gui/main_panel/main_container.py
+++ ninja_ide/gui/main_panel/main_container.py
@@ -481,8 +481,13 @@ class __MainContainer(QSplitter):
         if not editorWidget:
             return False
         try:
+            filter = '(*.py);;(*.*)'
+            if editorWidget.ID:
+                ext = file_manager.get_file_extension(editorWidget.ID)
+                if ext != 'py':
+                    filter = '(*.%s);;(*.py);;(*.*)' % ext
             fileName = unicode(QFileDialog.getSaveFileName(
-                self._parent, self.tr("Save File"), '', '(*.py);;(*.*)'))
+                self._parent, self.tr("Save File"), editorWidget.ID, filter))
             if not fileName:
                 return False
 
