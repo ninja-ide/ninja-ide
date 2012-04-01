2f488eaa0e8664a168503d8cb94fd61e107fa680
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2012-04-01 18:35:04 -0300

    Fixing ipc problem on windows, not working for windows, but doesn't block the application

--- ninja_ide/core/core.py
+++ ninja_ide/core/core.py
@@ -6,7 +6,6 @@ import signal
 
 from ninja_ide import resources
 from ninja_ide.core import cliparser
-from ninja_ide.core import ipc
 
 
 def run_ninja():
@@ -23,17 +22,9 @@ def run_ninja():
             print "The process couldn't be renamed'"
     #Set the application name
     filenames, projects_path, extra_plugins, linenos = cliparser.parse()
-    # Check if there is another session of ninja-ide opened
-    # and in that case send the filenames and projects to that session
-    running = ipc.is_running()
-    start_server = not running[0]
-    if running[0] and (filenames or projects_path):
-        sended = ipc.send_data(running[1], filenames, projects_path, linenos)
-        if sended:
-            sys.exit()
     # Create NINJA-IDE user folder structure for plugins, themes, etc
     resources.create_home_dir_structure()
 
     # Start the UI
     from ninja_ide.gui import ide
-    ide.start(filenames, projects_path, extra_plugins, linenos, start_server)
+    ide.start(filenames, projects_path, extra_plugins, linenos)
