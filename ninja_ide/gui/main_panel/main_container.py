4b01336cd5219428def801b24e4783f64602fdaf
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2012-05-16 17:32:58 -0300

    Show more relevant paths when opening a file

--- ninja_ide/gui/main_panel/main_container.py
+++ ninja_ide/gui/main_panel/main_container.py
@@ -23,6 +23,7 @@ from ninja_ide.gui.editor import highlighter
 from ninja_ide.gui.editor import helpers
 from ninja_ide.gui.main_panel import browser_widget
 from ninja_ide.gui.main_panel import image_viewer
+#from ninja_ide.gui import actions
 from ninja_ide.tools import runner
 
 
@@ -75,7 +76,6 @@ class __MainContainer(QSplitter):
         self.actualTab = self._tabMain
         self._followMode = False
         self.splitted = False
-        self._workingDirectory = ''
         highlighter.restyle(resources.CUSTOM_SCHEME)
         #documentation browser
         self.docPage = None
@@ -349,7 +349,13 @@ class __MainContainer(QSplitter):
             if settings.WORKSPACE:
                 directory = settings.WORKSPACE
             else:
-                directory = self._workingDirectory
+                directory = os.path.expanduser("~")
+                editorWidget = self.get_actual_editor()
+                current_project = self._parent.explorer.get_actual_project()
+                if current_project is not None:
+                    directory = current_project
+                elif type(editorWidget) is editor.Editor and editorWidget.ID:
+                    directory = file_manager.get_folder(editorWidget.ID)
             extensions = ';;'.join(
                 ['(*%s)' % e for e in \
                     settings.SUPPORTED_EXTENSIONS + ['.*', '']])
@@ -370,7 +376,6 @@ class __MainContainer(QSplitter):
             else:
                 self.__open_file(filename, cursorPosition,
                     tabIndex, positionIsLineNumber, notStart)
-        self._workingDirectory = os.path.dirname(unicode(fileNames[-1]))
 
     def __open_file(self, fileName='', cursorPosition=0,\
                     tabIndex=None, positionIsLineNumber=False, notStart=True):
