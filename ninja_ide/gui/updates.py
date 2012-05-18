434c025b8fdaad378ee935b6452c6ec5560045b3
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2012-05-18 20:44:02 -0300

    Version comparison improved using distutils.

--- ninja_ide/gui/updates.py
+++ ninja_ide/gui/updates.py
@@ -3,8 +3,11 @@
 import urllib
 import webbrowser
 import logging
+from distutils import version
 
 from PyQt4.QtGui import QSystemTrayIcon
+from PyQt4.QtGui import QAction
+from PyQt4.QtGui import QMenu
 from PyQt4.QtGui import QIcon
 from PyQt4.QtGui import QMessageBox
 from PyQt4.QtCore import QThread
@@ -25,26 +28,42 @@ class TrayIconUpdates(QSystemTrayIcon):
         QSystemTrayIcon.__init__(self, parent)
         icon = QIcon(resources.IMAGES['iconUpdate'])
         self.setIcon(icon)
+        self.setup_menu()
 
         if settings.NOTIFY_UPDATES:
             self.thread = ThreadUpdates()
 
-            self.connect(self, SIGNAL("messageClicked()"), self._show_download)
             self.connect(self.thread, SIGNAL("finished()"),
-                            self._show_messages)
+                self._show_messages)
             self.thread.start()
         else:
             self.show = self.hide
 
+    def setup_menu(self, show_downloads=False):
+        self.menu = QMenu()
+        if show_downloads:
+            self.download = QAction(self.tr("Download Version: %1!").arg(
+                self.thread.ide['version']),
+                self, triggered=self._show_download)
+            self.menu.addAction(self.download)
+            self.menu.addSeparator()
+        self.quit_action = QAction(self.tr("Close Update Notifications"),
+            self, triggered=self.hide)
+        self.menu.addAction(self.quit_action)
+
+        self.setContextMenu(self.menu)
+
     def _show_messages(self):
         try:
-            if self._convert_version_to_float(ninja_ide.__version__) \
-            < self._convert_version_to_float(self.thread.ide['version']):
+            local_version = version.LooseVersion(ninja_ide.__version__)
+            web_version = version.LooseVersion(self.thread.ide['version'])
+            if local_version < web_version:
                 if self.supportsMessages():
+                    self.setup_menu(True)
                     self.showMessage(self.tr("NINJA-IDE Updates"),
                         self.tr("New Version of NINJA-IDE\nAvailable: ") + \
-                        self.thread.ide['version'] +
-                        self.tr("\n\nClick here to Download"),
+                        self.thread.ide['version'] + \
+                        self.tr("\n\nCheck the Update Icon Menu to Download!"),
                         QSystemTrayIcon.Information, 10000)
                 else:
                     button = QMessageBox.information(self.parent(),
@@ -58,18 +77,6 @@ class TrayIconUpdates(QSystemTrayIcon):
         except:
             logger.warning('Versions can not be compared.')
             self.hide()
-
-    def _convert_version_to_float(self, version):
-        number = 0
-        if version.lower().find('beta') != -1:
-            number = float(version.split('-')[0]) - 0.02
-        elif version.lower().find('rc') != -1:
-            number = float(version.split('-')[0]) - 0.01
-        elif version.lower().find('dev') != -1:
-            number = float(version.split('-')[0]) - 0.03
-        else:
-            number = float(version)
-        return number
 
     def _show_download(self):
         webbrowser.open(self.thread.ide['downloads'])
