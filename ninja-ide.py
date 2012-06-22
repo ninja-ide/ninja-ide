b6dd84c8444d5c9d39288ce3d2582870849bbe00
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2012-06-22 16:18:46 -0300

    adding license

--- ninja-ide.py
+++ ninja-ide.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
-
 # -*- coding: utf-8 -*-
-
-#   This file is part of NINJA-IDE.
 #
-#    NINJA-IDE is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 3 of the License, or
-#    (at your option) any later version.
+# This file is part of NINJA-IDE (http://ninja-ide.org).
 #
-#    NINJA-IDE is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# NINJA-IDE is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with NINJA-IDE; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# NINJA-IDE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NINJA-IDE; If not, see <http://www.gnu.org/licenses/>.
 
 
 #==============================================================================
