074c543a5f1884c021b38bf32e988ee2ffadb600
Horacio Duran <hduran@machinalis.com>
2012-07-08 12:30:03 -0300

    Rewrote windows file changed detection
    Rewrote windows file changed detection as per what u1 people explained
    to me about FSs in windows
    Also improved a little tab widget handling of such events.

--- ninja_ide/core/filesystem_notifications/windows.py
+++ ninja_ide/core/filesystem_notifications/windows.py
@@ -10,7 +10,7 @@
 # NINJA-IDE is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU General Public License for more details. #
 #
 # You should have received a copy of the GNU General Public License
 # along with NINJA-IDE; If not, see <http://www.gnu.org/licenses/>.
@@ -22,6 +22,7 @@ from threading import Thread
 import win32con
 import win32file
 import win32event
+import pywintypes
 import os
 from ninja_ide.core import file_manager
 
@@ -36,8 +37,19 @@ REMOVE = base_watcher.REMOVE
 RENAME = base_watcher.RENAME
 MODIFIED = base_watcher.MODIFIED
 
+ACTIONS = {
+  1: ADDED,
+  2: DELETED,
+  3: MODIFIED,
+  4: RENAME,
+  5: RENAME
+}
+
+# Thanks to Claudio Grondi for the correct set of numbers
+FILE_LIST_DIRECTORY = 0x0001
+
 watchmask = win32con.FILE_NOTIFY_CHANGE_FILE_NAME | \
-            win32con.FILE_NOTIFY_CHANGE_SIZE | \
+            win32con.FILE_NOTIFY_CHANGE_LAST_WRITE | \
             win32con.FILE_NOTIFY_CHANGE_DIR_NAME
 
 
@@ -59,7 +71,11 @@ class FileEventCallback(object):
         for path in paths:
             self.snapshot(path)
         self.callback = callback
+        self._path_last_time = {}
         self.cookie = 0
+
+    def pulentastack(self, path):
+        return os.stat(path)
 
     def __call__(self, paths):
         events = []
@@ -68,12 +84,12 @@ class FileEventCallback(object):
         for path in sorted(paths):
             path = path.rstrip('/')
             snapshot = self.snapshots[path]
-
             current = {}
             try:
                 for name in listdir(path):
                     try:
-                        current[name] = os.stat(os.path.join(path, name))
+                        current[name] = self.pulentastack(os.path.join(path,
+                                                                        name))
                     except OSError:
                         pass
             except OSError:
@@ -95,21 +111,22 @@ class FileEventCallback(object):
                     events.append(event)
 
             for name in observed:
-                stat = current[name]
-                filename = os.path.join(path, name)
+                if name != path:
+                    stat = current[name]
+                    filename = os.path.join(path, name)
 
-                event = deleted.get(stat.st_ino)
-                if event is not None:
-                    event = (REMOVE, filename)
-                else:
-                    event = (ADDED, filename)
+                    event = deleted.get(stat.st_ino)
+                    if event is not None:
+                        event = (REMOVE, filename)
+                    else:
+                        event = (ADDED, filename)
 
-                if os.path.isdir(filename):
-                    self.snapshot(filename)
+                    if os.path.isdir(filename):
+                        self.snapshot(filename)
 
                 events.append(event)
-            snapshot.clear()
-            snapshot.update(current)
+        snapshot.clear()
+        snapshot.update(current)
 
         for event in events:
             self.callback(*event)
@@ -123,7 +140,8 @@ class FileEventCallback(object):
             entry = refs[root]
             for filename in files:
                 try:
-                    entry[filename] = os.stat(os.path.join(root, filename))
+                    entry[filename] = self.pulentastack(os.path.join(root,
+                                                                    filename))
                 except OSError:
                     continue
             for directory in dirs:
@@ -133,7 +151,8 @@ class FileEventCallback(object):
             refs[os.path.join(root, path)] = {}
             for name in listdir(os.path.join(root, path)):
                 try:
-                    refs[path][name] = os.stat(os.path.join(path, name))
+                    refs[path][name] = self.pulentastack(os.path.join(path,
+                                                                        name))
                 except OSError:
                     pass
 
@@ -142,20 +161,68 @@ class ThreadedFSWatcher(Thread):
 
     def __init__(self, path, callback):
         self._watch_path = path
-        self._callback = FileEventCallback(callback, (path, ))
+        self._callback = callback  # FileEventCallback(callback, (path, ))
         self._windows_sucks_flag = True
+        self._wait_stop = win32event.CreateEvent(None, 0, 0, None)
+        self._overlapped = pywintypes.OVERLAPPED()
+        self._overlapped.hEvent = win32event.CreateEvent(None, 0, 0, None)
         super(ThreadedFSWatcher, self).__init__()
 
     def stop(self):
         self._windows_sucks_flag = False
+        win32event.SetEvent(self._wait_stop)
 
     def run(self):
-        change_handle = win32file.FindFirstChangeNotification(self._watch_path,
-                                                              True, watchmask)
+        hDir = win32file.CreateFileW(
+                self._watch_path,
+                FILE_LIST_DIRECTORY,
+                win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE,
+                None,
+                win32con.OPEN_EXISTING,
+                win32con.FILE_FLAG_BACKUP_SEMANTICS |
+                win32con.FILE_FLAG_OVERLAPPED,
+                None
+            )
         while self._windows_sucks_flag:
-            result = win32event.WaitForSingleObject(change_handle, 500)
-            if result == win32con.WAIT_OBJECT_0:
-                self._callback((self._watch_path, ))
+            buf = win32file.AllocateReadBuffer(1024)
+            win32file.ReadDirectoryChangesW(
+                hDir,
+                buf,
+                True,
+                win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
+                win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
+                win32con.FILE_NOTIFY_CHANGE_SIZE |
+                win32con.FILE_NOTIFY_CHANGE_LAST_WRITE,
+                self._overlapped
+              )
+            result_stack = {}
+            rc = win32event.WaitForMultipleObjects((self._wait_stop,
+                                         self._overlapped.hEvent),
+                                         0, win32event.INFINITE)
+            if rc == win32event.WAIT_OBJECT_0:
+                # Stop event
+                break
+
+            data = win32file.GetOverlappedResult(hDir, self._overlapped, True)
+            # lets read the data and store it in the results
+            results = win32file.FILE_NOTIFY_INFORMATION(buf, data)
+
+            for action, afile in results:
+                if action in ACTIONS:
+                    full_filename = os.path.join(self._watch_path, afile)
+                    result_stack.setdefault(full_filename,
+                                         []).append(ACTIONS.get(action))
+            keys = result_stack.keys()
+            while len(keys):
+                key = keys.pop(0)
+                event = result_stack.pop(key)
+                if (ADDED in event) and (DELETED in event):
+                    event = [e for e in event if e not in (ADDED, DELETED)]
+                noticed = []
+                for each_event in event:
+                    if each_event not in noticed:
+                        self._callback(each_event, full_filename)
+                        noticed.append(each_event)
 
 
 class NinjaFileSystemWatcher(base_watcher.BaseWatcher):
@@ -187,4 +254,5 @@ class NinjaFileSystemWatcher(base_watcher.BaseWatcher):
             each_path.join()
 
     def _emit_signal_on_change(self, event, path):
+        DEBUG("About to emit the signal")
         self.emit(SIGNAL("fileChanged(int, QString)"), event, path)
