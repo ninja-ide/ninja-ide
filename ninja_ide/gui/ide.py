cbe846734d8238f1bb34994b9835a7e736c4a431
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2012-09-08 21:26:41 -0300

    fixing merge

--- ninja_ide/gui/ide.py
+++ ninja_ide/gui/ide.py
@@ -274,15 +274,11 @@ class __IDE(QMainWindow):
         centralWidget.insert_bottom_container(self.misc)
         self.connect(self.mainContainer,
             SIGNAL("cursorPositionChange(int, int)"),
-<<<<<<< HEAD
-            self.central.lateralPanel.update_line_col)
+            self.central.lateralDock.update_line_col)
         # TODO: Change current symbol on move
         #self.connect(self.mainContainer,
             #SIGNAL("cursorPositionChange(int, int)"),
             #self.explorer.update_current_symbol)
-=======
-            self.central.lateralDock.update_line_col)
->>>>>>> upstream/master
         self.connect(self.mainContainer, SIGNAL("enabledFollowMode(bool)"),
             self.central.enable_follow_mode_scrollbar)
 
