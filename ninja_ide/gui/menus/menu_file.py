c4485ad82a49b6a1db9865813e2d43d53797d2db
Erik Tollerud <erik.tollerud@gmail.com>
2012-10-08 15:00:04 -0400

    Fix OS X start failures by self.tr -> self.trUtf8

--- ninja_ide/gui/menus/menu_file.py
+++ ninja_ide/gui/menus/menu_file.py
@@ -39,69 +39,69 @@ class MenuFile(QObject):
         QObject.__init__(self)
 
         newAction = menuFile.addAction(QIcon(resources.IMAGES['new']),
-            self.tr("&New File (%s)" %
+            self.trUtf8("&New File (%s)" %
                 resources.get_shortcut("New-file").toString(
                     QKeySequence.NativeText)))
         newProjectAction = menuFile.addAction(
             QIcon(resources.IMAGES['newProj']),
-            self.tr("New Pro&ject (%s)" %
+            self.trUtf8("New Pro&ject (%s)" %
                 resources.get_shortcut("New-project").toString(
                     QKeySequence.NativeText)))
         menuFile.addSeparator()
         saveAction = menuFile.addAction(QIcon(resources.IMAGES['save']),
-            self.tr("&Save (%s)" %
+            self.trUtf8("&Save (%s)" %
                 resources.get_shortcut("Save-file").toString(
                     QKeySequence.NativeText)))
         saveAsAction = menuFile.addAction(QIcon(resources.IMAGES['saveAs']),
-            self.tr("Save &As"))
+            self.trUtf8("Save &As"))
         saveAllAction = menuFile.addAction(QIcon(resources.IMAGES['saveAll']),
-            self.tr("Save All"))
+            self.trUtf8("Save All"))
         saveProjectAction = menuFile.addAction(QIcon(
             resources.IMAGES['saveAll']),
-            self.tr("Save Pro&ject  (%s)" %
+            self.trUtf8("Save Pro&ject  (%s)" %
                 resources.get_shortcut("Save-project").toString(
                     QKeySequence.NativeText)))
         menuFile.addSeparator()
         reloadFileAction = menuFile.addAction(
             QIcon(resources.IMAGES['reload-file']),
-            self.tr("Reload File (%s)" %
+            self.trUtf8("Reload File (%s)" %
                 resources.get_shortcut("Reload-file").toString(
                     QKeySequence.NativeText)))
         menuFile.addSeparator()
         openAction = menuFile.addAction(QIcon(resources.IMAGES['open']),
-            self.tr("&Open (%s)" %
+            self.trUtf8("&Open (%s)" %
                 resources.get_shortcut("Open-file").toString(
                     QKeySequence.NativeText)))
         openProjectAction = menuFile.addAction(
             QIcon(resources.IMAGES['openProj']),
-            self.tr("Open &Project (%s)" %
+            self.trUtf8("Open &Project (%s)" %
                 resources.get_shortcut("Open-project").toString(
                     QKeySequence.NativeText)))
-        self.recent_files = menuFile.addMenu(self.tr('Open Recent Files'))
+        self.recent_files = menuFile.addMenu(self.trUtf8('Open Recent Files'))
         menuFile.addSeparator()
         activateProfileAction = menuFile.addAction(
             QIcon(resources.IMAGES['activate-profile']),
-            self.tr("Activate Profile"))
+            self.trUtf8("Activate Profile"))
         deactivateProfileAction = menuFile.addAction(
             QIcon(resources.IMAGES['deactivate-profile']),
-            self.tr("Deactivate Profile"))
+            self.trUtf8("Deactivate Profile"))
         menuFile.addSeparator()
         printFile = menuFile.addAction(QIcon(resources.IMAGES['print']),
-            self.tr("Pr&int File (%s)" %
+            self.trUtf8("Pr&int File (%s)" %
                 resources.get_shortcut("Print-file").toString(
                     QKeySequence.NativeText)))
         closeAction = menuFile.addAction(
             ide.style().standardIcon(QStyle.SP_DialogCloseButton),
-            self.tr("&Close Tab (%s)" %
+            self.trUtf8("&Close Tab (%s)" %
                 resources.get_shortcut("Close-tab").toString(
                     QKeySequence.NativeText)))
         closeProjectsAction = menuFile.addAction(
             ide.style().standardIcon(QStyle.SP_DialogCloseButton),
-            self.tr("&Close All Projects"))
+            self.trUtf8("&Close All Projects"))
         menuFile.addSeparator()
         exitAction = menuFile.addAction(
             ide.style().standardIcon(QStyle.SP_DialogCloseButton),
-            self.tr("&Exit"))
+            self.trUtf8("&Exit"))
 
         self.toolbar_items = {
             'new-file': newAction,
