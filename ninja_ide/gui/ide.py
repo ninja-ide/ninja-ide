1aa0cab85fe57b0fb78e6e4d827febb8359162a3
Martin N. Alderete <malderete@gmail.com>
2012-10-09 00:05:16 -0300

    Fixed issue #903

--- ninja_ide/gui/ide.py
+++ ninja_ide/gui/ide.py
@@ -416,9 +416,9 @@ class __IDE(QMainWindow):
             self.setWindowState(Qt.WindowMaximized)
         else:
             self.resize(qsettings.value("window/size",
-                QSize(800, 600)).toSize())
+                QSize(800, 600)))
             self.move(qsettings.value("window/pos",
-                QPoint(100, 100)).toPoint())
+                QPoint(100, 100)))
 
     def closeEvent(self, event):
         if self.s_listener:
