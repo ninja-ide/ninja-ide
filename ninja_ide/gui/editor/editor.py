6459fd63e10af9b556cb92b6ae4ffbbfed896685
Joseph Daywalt <joseph.daywalt@live.com>
2012-11-06 19:48:08 -0500

    Reverted current-line highlight alpha to 20 (out of 255) instead of 255

--- ninja_ide/gui/editor/editor.py
+++ ninja_ide/gui/editor/editor.py
@@ -1073,7 +1073,7 @@ class Editor(QPlainTextEdit, itab_item.ITabItem):
             selection = QTextEdit.ExtraSelection()
             lineColor = QColor(resources.CUSTOM_SCHEME.get('current-line',
                         resources.COLOR_SCHEME['current-line']))
-            lineColor.setAlpha(255)
+            lineColor.setAlpha(20)
             selection.format.setBackground(lineColor)
             selection.format.setProperty(QTextFormat.FullWidthSelection, True)
             selection.cursor = self.textCursor()
