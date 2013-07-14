387c87100ef2f19281129faabfcf26849baec2ae
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2013-07-14 17:33:26 -0300

    adding dynamic splitter

---
+++ ninja_ide/gui/dynamic_splitter.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from PyQt4.QtGui import QSplitter
+from PyQt4.QtCore import Qt
+
+
+class DynamicSplitter(QSplitter):
+
+    def __init__(self, orientation):
+        super(DynamicSplitter, self).__init__(orientation)
+
+    def insertWidget(self, index, widget):
+        current = self.widget(index)
+        if not current:
+            super(DynamicSplitter, self).insertWidget(index, widget)
+        elif isinstance(current, DynamicSplitter):
+            current.insertWidget(index, widget)
+        else:
+            splitter = DynamicSplitter(Qt.Vertical)
+            splitter.addWidget(current)
+            splitter.addWidget(widget)
+            super(DynamicSplitter, self).insertWidget(index, splitter)
+
+    def addWidget(self, widget):
+        self.insertWidget(1, widget)
