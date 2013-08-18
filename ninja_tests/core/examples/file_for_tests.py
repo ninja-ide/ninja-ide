c0f1037eb2f6659d14bd5e083245030600bd8c1a
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2013-08-18 09:25:26 -0300

    'documentator deploy'

--- ninja_tests/core/examples/file_for_tests.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-print 'testing'
-print 'ñandú testing'
--- ninja_tests/core/plugins/test_plugin/test_plugin.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from ninja_ide.core import plugin
-
-
-class TestPlugin(plugin.Plugin):
-    pass
