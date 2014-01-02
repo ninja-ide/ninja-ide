2b9e4d1f8a615ba8e003d7332eecd0efd1145ea2
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2014-01-02 19:04:43 -0300

    update checkers display info when current file change

--- ninja_ide/gui/editor/neditable.py
+++ ninja_ide/gui/editor/neditable.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+#
+# This file is part of NINJA-IDE (http://ninja-ide.org).
+#
+# NINJA-IDE is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# NINJA-IDE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NINJA-IDE; If not, see <http://www.gnu.org/licenses/>.
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import collections
 
 from PyQt4.QtCore import QObject
 from PyQt4.QtCore import SIGNAL
@@ -157,3 +176,10 @@ class NEditable(QObject):
         if self._checkers_executed == len(self.registered_checkers):
             self._checkers_executed = 0
             self.emit(SIGNAL("checkersUpdated(PyQt_PyObject)"), self)
+
+    def update_checkers_display(self):
+        for items in self.registered_checkers:
+            checker, _, _ = items
+            func = getattr(checker, 'refresh_display', None)
+            if isinstance(func, collections.Callable):
+                func()
