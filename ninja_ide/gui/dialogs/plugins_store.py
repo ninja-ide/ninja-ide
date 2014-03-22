886ab018442d7c8e4410525046a3db466f8a8b9d
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2014-03-22 19:15:05 -0300

    refactoring plugins and working on plugins store

---
+++ ninja_ide/gui/dialogs/plugins_store.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of NINJA-IDE (http://ninja-ide.org).
+#
+# NINJA-IDE is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# NINJA-IDE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NINJA-IDE; If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt4.QtGui import QDialog
+from PyQt4.QtGui import QVBoxLayout
+from PyQt4.QtDeclarative import QDeclarativeView
+from PyQt4.QtCore import Qt
+
+from ninja_ide.tools import ui_tools
+
+
+class PluginsStore(QDialog):
+
+    def __init__(self, parent=None):
+        super(PluginsStore, self).__init__(parent, Qt.Dialog)
+        vbox = QVBoxLayout(self)
+        vbox.setContentsMargins(0, 0, 0, 0)
+        vbox.setSpacing(0)
+        self.view = QDeclarativeView()
+        self.view.setMinimumWidth(800)
+        self.view.setMinimumHeight(600)
+        self.view.setResizeMode(QDeclarativeView.SizeRootObjectToView)
+        self.view.setSource(ui_tools.get_qml_resource("PluginsStore.qml"))
+        self.root = self.view.rootObject()
+        vbox.addWidget(self.view)
--- ninja_ide/intellisensei/analyzer/__init__.py
+++ ninja_ide/intellisensei/analyzer/__init__.py
@@ -14,6 +14,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with NINJA-IDE; If not, see <http://www.gnu.org/licenses/>.
-
-
-__version__ = "0.1"
