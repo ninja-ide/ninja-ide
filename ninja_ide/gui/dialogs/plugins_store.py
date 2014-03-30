e7269a9dca8c9e59e4a7f9ad1afc903e4f114faa
Diego Sarmentero <diego.sarmentero@ninja-ide.org>
2014-03-29 23:06:00 -0300

    Plugin details, install animation and tags/author search working

--- ninja_ide/gui/dialogs/plugins_store.py
+++ ninja_ide/gui/dialogs/plugins_store.py
@@ -15,8 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with NINJA-IDE; If not, see <http://www.gnu.org/licenses/>.
 
+import collections
+import random
+
 from PyQt4.QtGui import QDialog
 from PyQt4.QtGui import QVBoxLayout
+from PyQt4.QtGui import QColor
 from PyQt4.QtDeclarative import QDeclarativeView
 from PyQt4.QtCore import Qt
 from PyQt4.QtCore import SIGNAL
@@ -43,30 +47,149 @@ class PluginsStore(QDialog):
         vbox.addWidget(self.view)
         self._plugins = {}
         self._plugins_inflate = []
+        self._plugins_by_tag = collections.defaultdict(list)
+        self._plugins_by_author = collections.defaultdict(list)
+        self._base_color = QColor("white")
+        self._counter = 0
+        self._counter_callback = None
+        self._inflating_plugins = []
+        self._categoryTags = True
 
         self.nenv = nenvironment.NenvEggSearcher()
         self.connect(self.nenv, SIGNAL("searchCompleted(PyQt_PyObject)"),
                      self.callback)
-        self.connect(self.root, SIGNAL("inflatePlugin(int)"),
-                     self._update_plugin_metadata)
+        self.connect(self.root, SIGNAL("loadPluginsGrid()"),
+                     self._load_by_name)
+        self.connect(self.root, SIGNAL("showPluginDetails(int)"),
+                     self.show_plugin_details)
+        self.connect(self.root, SIGNAL("loadTagsGrid()"),
+                     self._load_tags_grid)
+        self.connect(self.root, SIGNAL("loadAuthorGrid()"),
+                     self._load_author_grid)
+        self.connect(self.root, SIGNAL("loadPluginsForCategory(QString)"),
+                     self._load_plugins_for_category)
+        self.connect(self, SIGNAL("processCompleted(PyQt_PyObject)"),
+                     self._process_complete)
 
         self.status = self.nenv.do_search()
 
+    def _load_by_name(self):
+        if self._plugins:
+            self.root.showGridPlugins()
+            for plugin in list(self._plugins.values()):
+                self.root.addPlugin(plugin.identifier, plugin.name,
+                                    plugin.summary, plugin.version)
+
+    def _load_plugins_for_category(self, name):
+        self.root.showGridPlugins()
+        if self._categoryTags:
+            for plugin in self._plugins_by_tag[name]:
+                self.root.addPlugin(plugin.identifier, plugin.name,
+                                    plugin.summary, plugin.version)
+        else:
+            for plugin in self._plugins_by_author[name]:
+                self.root.addPlugin(plugin.identifier, plugin.name,
+                                    plugin.summary, plugin.version)
+
     def callback(self, values):
-        self.root.showGrid()
+        self.root.showGridPlugins()
         for i, plugin in enumerate(values):
             plugin.identifier = i + 1
             self.root.addPlugin(plugin.identifier, plugin.name,
                                 plugin.summary, plugin.version)
             self._plugins[plugin.identifier] = plugin
 
-    def _update_plugin_metadata(self, identifier):
-        pluginShallow = self._plugins[identifier]
+    def show_plugin_details(self, identifier):
+        plugin = self._plugins[identifier]
+        self._counter = 1
+        self._counter_callback = self._show_details
 
-        def update_content(plugin):
-            self.root.updatePlugin(plugin.identifier, plugin.author)
+        if plugin.shallow:
+            self.connect(plugin,
+                         SIGNAL("pluginMetadataInflated(PyQt_PyObject)"),
+                         self._update_content)
+            self._plugins_inflate.append(plugin.inflate())
+        else:
+            self._update_content(plugin)
 
-        self.connect(pluginShallow,
-                     SIGNAL("pluginMetadataInflated(PyQt_PyObject)"),
-                     update_content)
-        self._plugins_inflate.append(pluginShallow.inflate())
+    def _load_tags_grid(self):
+        self._categoryTags = True
+        self._counter = len(self._plugins)
+        self.root.updateCategoryCounter(self._counter)
+        self._counter_callback = self._show_tags_grid
+        self._inflating_plugins = list(self._plugins.values())
+        self._loading_function()
+
+    def _load_author_grid(self):
+        self._categoryTags = False
+        self._counter = len(self._plugins)
+        self.root.updateCategoryCounter(self._counter)
+        self._counter_callback = self._show_author_grid
+        self._inflating_plugins = list(self._plugins.values())
+        self._loading_function()
+
+    def _loading_function(self):
+        plugin = self._inflating_plugins.pop()
+        if plugin.shallow:
+            self.connect(plugin,
+                         SIGNAL("pluginMetadataInflated(PyQt_PyObject)"),
+                         self._update_content)
+            self._plugins_inflate.append(plugin.inflate())
+        else:
+            self._process_complete(plugin)
+
+    def _process_complete(self, plugin=None):
+        self._counter -= 1
+        self.root.updateCategoryCounter(self._counter)
+        if self._counter == 0:
+            self._counter_callback(plugin)
+        else:
+            self._loading_function()
+
+    def _show_details(self, plugin):
+        self.root.displayDetails(plugin.identifier)
+
+    def _show_tags_grid(self, plugin=None):
+        tags = sorted(self._plugins_by_tag.keys())
+        for tag in tags:
+            color = self._get_random_color(self._base_color)
+            self.root.addCategory(color.name(), tag)
+        self.root.loadingComplete()
+
+    def _show_author_grid(self, plugin=None):
+        authors = sorted(self._plugins_by_author.keys())
+        for author in authors:
+            color = self._get_random_color(self._base_color)
+            self.root.addCategory(color.name(), author)
+        self.root.loadingComplete()
+
+    def _update_content(self, plugin):
+        self.root.updatePlugin(
+            plugin.identifier, plugin.author, plugin.author_email,
+            plugin.description, plugin.download_url, plugin.home_page,
+            plugin.license)
+        keywords = plugin.keywords.split()
+        for key in keywords:
+            plugins = self._plugins_by_tag[key]
+            if plugin not in plugins:
+                plugins.append(plugin)
+                self._plugins_by_tag[key] = plugins
+        plugins = self._plugins_by_author[plugin.author]
+        if plugin not in plugins:
+            plugins.append(plugin)
+            self._plugins_by_author[plugin.author] = plugins
+        self.emit(SIGNAL("processCompleted(PyQt_PyObject)"), plugin)
+
+    def _get_random_color(self, mix=None):
+        red = random.randint(0, 256)
+        green = random.randint(0, 256)
+        blue = random.randint(0, 256)
+
+        # mix the color
+        if mix:
+            red = (red + mix.red()) / 2
+            green = (green + mix.green()) / 2
+            blue = (blue + mix.blue()) / 2
+
+        color = QColor(red, green, blue)
+        return color
