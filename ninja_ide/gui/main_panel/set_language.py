e49ff7029a93a0b2ab52cbb1487a15cac70be2a7
earias <earias@vengsa.com.ar>
2017-12-20 09:00:34 -0300

    Hago uso del syntaxhighlighter Elimino los dict del set_languge.py, para que mire los dict que hay en syntaxhighligher.py. Me parece mejor asi ya que queda concentrado los lenguajes que el ide maneja en un solo lugar.

--- ninja_ide/gui/main_panel/set_language.py
+++ ninja_ide/gui/main_panel/set_language.py
@@ -19,63 +19,10 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from ninja_ide.gui.ide import IDE
+from ninja_ide.gui.editor import syntaxhighlighter as SH
 
 
 class SetLanguageFile(object):
-    def __init__(self):
-        self.dict_language = {
-            0: None,  # This is the default value, maybe need some work
-            1: 'python',
-            # 2: 'HTML',
-            # 3: 'js',
-            # 4: 'qml'
-        }
-        self.LEXER_MAP = {
-            "asm": "assembler",
-            "json": "json",
-            "cs": "csharp",
-            "rb": "ruby_on_rails",
-            "cpp": "cpp",
-            "coffee": "coffeescript",
-            "tex": "bibtex",
-            "js": "javascript",
-            "qml": "javascript",
-            "mo": "gettext",
-            "po": "gettext",
-            "pot": "gettext",
-            "v": "verilog",
-            "sh": "shell",
-            "shell": "shell",
-            "bash": "shell",
-            "ksh": "shell",
-            "pas": "pascal",
-            "html": "html",
-            "list": "sourceslist",
-            "lol": "lolcode",
-            "h": "header",
-            "conf": "apache",
-            "php": "php",
-            "php4": "php",
-            "php5": "php",
-            "css": "css",
-            "qss": "css",
-            "scss": "css",
-            "sass": "css",
-            "tex": "latex",
-            "py": "python",
-            "pyw": "python",
-            "rpy": "python",
-            "tac": "python",
-            "pyx": "cython",
-            "pxd": "cython",
-            "pxi": "cython",
-            "go": "go",
-            "asp": "asp",
-            "rst": "rst",
-            "c": "c",
-            "java": "java",
-        }
-
     def set_language_to_editor(self, lang):
         self.mc = IDE.get_service("main_container")
         self._current_editor_widget = self.mc.get_current_editor()
@@ -84,12 +31,12 @@ class SetLanguageFile(object):
         #    self.dict_language[index])
 
     def get_list_of_language(self):
-        return self.dict_language.values()
+        return SH.SYNTAXES.keys()
 
     def get_lang(self, extension):
         if not extension:
             extension = "py"
-        return self.LEXER_MAP.get(extension, "")
+        return SH.LANGUAGE_MAP.get(extension, "")
 
     def set_language_from_extension(self, ext):
         self.mc = IDE.get_service("main_container")
