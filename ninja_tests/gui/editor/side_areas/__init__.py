24d5f8b3bccda9a5514d4f414cb279ba2418f843
Gabriel Acosta <gabriel.acosta@ninja-ide.org>
2018-03-05 14:48:44 -0300

    Fix test gui

--- ninja_tests/gui/editor/side_areas/__init__.py
+++ ninja_tests/gui/editor/side_areas/__init__.py
@@ -1,5 +1,9 @@
 from ninja_ide.gui.editor.side_area import marker_widget
-from ninja_tests.gui.editor import create_editor
+# Need COLOR_SCHEME
+from ninja_ide import resources
+from ninja_ide.tools import json_manager
+all_schemes = json_manager.load_editor_schemes()
+resources.COLOR_SCHEME = all_schemes["Ninja Dark"]
 
 
 def get_marker_area():
