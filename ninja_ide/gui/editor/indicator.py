327c0993fac523ac3ba81bff82649c63bf02225d
Gabriel Acosta <acostadariogabriel@gmail.com>
2018-06-20 09:39:57 -0300

    Add license in indicator.py

--- ninja_ide/gui/editor/indicator.py
+++ ninja_ide/gui/editor/indicator.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of NINJA-IDE (http://ninja-ide.org).
+#
+# NINJA-IDE is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# NINJA-IDE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NINJA-IDE; If not, see <http://www.gnu.org/licenses/>.
 
 from PyQt5.QtWidgets import QGraphicsOpacityEffect
 from PyQt5.QtWidgets import QHBoxLayout
@@ -26,9 +42,6 @@ class FadingIndicator(QFrame):
         font = self._label.font()
         font.setPixelSize(18)
         self._label.setFont(font)
-        palette = self.palette()
-        palette.setColor(QPalette.WindowText, palette.color(QPalette.Window))
-        # self._label.setPalette(palette)
 
         self._layout = QHBoxLayout(self)
         self._layout.addWidget(self._label)
