16773ae2a63daf14118841cc8872535ba9522fb7
Gabriel Acosta <acostadariogabriel@gmail.com>
2018-07-27 04:09:02 -0300

    Fix execute file when is not saved

--- ninja_ide/gui/tools_dock/run_widget.py
+++ ninja_ide/gui/tools_dock/run_widget.py
@@ -352,7 +352,8 @@ class RunWidget(QWidget):
         editor_widget = main_container.get_current_editor()
         if editor_widget is not None and (editor_widget.is_modified or
                                           editor_widget.file_path):
-            main_container.save_file(editor_widget)
+            if not main_container.save_file(editor_widget):
+                return
             # FIXME: Emit a signal for plugin!
             # self.fileExecuted.emit(editor_widget.file_path)
             file_path = editor_widget.file_path
